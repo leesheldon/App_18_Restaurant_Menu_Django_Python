43de22efa19c8d1666b77f32ba5e0d7f6866c4f3
Sheldon <lhanco@gmail.com>
2024-04-04 22:27:59 +0700

    Day 55: Create the database model.

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+MEAL_TYPE = (
+    ("starters", "Starters"),
+    ("salads", "Salads"),
+    ("main_dishes", "Main Dishes"),
+    ("desserts", "Desserts")
+)
+
+MEAL_STATUS = (
+    (0, "Unavailable"),
+    (1, "Available")
+)
+
+
+class Meal(models.Model):
+    meal_name = models.CharField(max_length=1000, unique=True)
+    description = models.CharField(max_length=2000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    meal_type = models.CharField(max_length=200, choices=MEAL_TYPE)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.IntegerField(choices=MEAL_STATUS, default=1)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.meal_name
