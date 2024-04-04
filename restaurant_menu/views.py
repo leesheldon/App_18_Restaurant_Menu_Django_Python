2f6e73b2ffddf1778eb127dcd637b3aa23a1e555
Sheldon <lhanco@gmail.com>
2024-04-04 23:10:10 +0700

    Day 56: Create Class-based views.

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Meal
 
-# Create your views here.
+
+class MenuList(generic.ListView):
+    queryset = Meal.objects.order_by("-created_on")
+    template_name = "index.html"
+
+
+class MenuItemDetail(generic.DetailView):
+    model = Meal
+    template_name = "menu_item_detail.html"
+
