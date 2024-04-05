bb3a68136e4fa37495ba3d467eafb67b559b722a
Sheldon <lhanco@gmail.com>
2024-04-05 10:42:29 +0700

    Day 57: Create the Admin interface and add data.

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Meal
 
-# Register your models here.
+
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("meal_name", "meal_type", "status")
+    list_filter = ("status", "meal_type")
+    search_fields = ("meal_name", "description")
+
+
+admin.site.register(Meal, MenuItemAdmin)
