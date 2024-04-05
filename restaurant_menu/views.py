93c9c17dd574030086ca6b2d8c4926b1f1135fb6
Sheldon <lhanco@gmail.com>
2024-04-05 11:17:31 +0700

    Day 57: Show data in Menu List view.

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,14 +1,60 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Meal
+from .models import Meal, MEAL_TYPE
 
 
 class MenuList(generic.ListView):
     queryset = Meal.objects.order_by("-created_on")
     template_name = "index.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meal_types"] = MEAL_TYPE
+        return context
+
 
 class MenuItemDetail(generic.DetailView):
     model = Meal
     template_name = "menu_item_detail.html"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
